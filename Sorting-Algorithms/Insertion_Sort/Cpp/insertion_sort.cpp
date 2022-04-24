da3b2451eb3166f720798b36d73dc2ba0ecaf409
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-04-24 18:08:44 +0530

    Create insertion_sort.cpp

---
+++ Sorting-Algorithms/Insertion_Sort/Cpp/insertion_sort.cpp
@@ -0,0 +1,35 @@
+/*
+    Insertion Sort
+    Time Complexity
+        Best Case Scenario  : O(n)
+        Worst Case Scenario : O(n^2)
+*/
+#include <iostream>
+using namespace std;
+void insertion_sort(int *A,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int key=A[i];
+        int j=i-1;
+        while(j>=0 && A[j]>key)
+        {
+            A[j+1]=A[j];
+            j--;
+        }
+        A[j+1]=key;
+    }
+}
+int main()
+{
+    int arr[]={1,-10,-30,20,2,11,15};
+    int n=sizeof(arr)/sizeof(arr[0]),i;
+    insertion_sort(arr,n);
+    cout<<"Array after bubble sort : ";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
