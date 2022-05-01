9957bf9769d77607be9f8518861caf7f3cc1534a
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-05-01 20:57:46 +0530

    Create bubble_sort.py

---
+++ Sorting-Algorithms/Bubble_Sort/Python/bubble_sort.py
@@ -0,0 +1,20 @@
+'''
+    Bubble Sort
+    Time Complexity
+        Best Case Scenario  : O(n)
+        Worst Case Scenario : O(n^2)
+'''
+def bubble_sort(arr,n):
+    for i in range(0,n-1):
+        for j in range(0,n-i-1):
+            if(arr[j]>arr[j+1]):
+                arr[j+1],arr[j]=arr[j],arr[j+1]
+
+arr=[1,-10,-30,20,2,11,15]
+n=len(arr)
+bubble_sort(arr,n)
+print("Array after bubble sort : ", end='')
+for i in arr:
+    print("{} ".format(i),end="")
+
+print()
